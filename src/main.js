856102f4f0570dbd109f497b0e0a3a4961affd4c
vik17ag <vik17ag@gmail.com>
2021-05-08 21:04:54 +0530

    dependency: Adds i18n

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueTypedJs from 'vue-typed-js'
+import i18n from './i18n'
 Vue.use(VueTypedJs);
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
