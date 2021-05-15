cfd0a0a013e1359e33aa131fe58780eec80ff2a2
vik17ag <vik17ag@gmail.com>
2021-05-15 20:52:25 +0530

    feat: Adds fields in morning

--- src/views/Morning.vue
+++ src/views/Morning.vue
@@ -29,81 +29,69 @@
         active-class=""
       >
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox  v-model="Gargles"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Gargles</v-list-item-title>
               <v-list-item-subtitle>Gargling with warm salt water will help ease the discomfort and irritation caused by a dry cough.</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox  v-model="Medicine"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Medicine</v-list-item-title>
               <v-list-item-subtitle>Take Prescribed Medicines on Time</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox  v-model="Exercises"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Breathing Exercises</v-list-item-title>
               <v-list-item-subtitle>Deep breathing can help reduce stress & increase lung capacity.</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox  v-model="Temperature"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Body Temperature</v-list-item-title>
               <v-list-item-subtitle>Take your temperature via thermometer or Infra Scanner</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox  v-model="Pressure"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Blood Pressure</v-list-item-title>
               <v-list-item-subtitle>Monitor BP If Prescribed by your doctor</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox  :input-value="active"></v-checkbox>
+              <v-checkbox  v-model="pulse"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Pulse Rate</v-list-item-title>
               <v-list-item-subtitle>Monitor Pulse rate as prescribed by your doctor</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
         <v-list-item>
@@ -118,16 +106,14 @@
         </v-list-item>
 
         <v-list-item>
-          <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox v-model="Breakfast"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
               <v-list-item-title>Breakfast</v-list-item-title>
               <v-list-item-subtitle>Have a nutritious breakfast on time focusing on whole foods</v-list-item-subtitle>
             </v-list-item-content>
-          </template>
         </v-list-item>
 
 
@@ -143,7 +129,13 @@ export default {
   name: 'Morning',
   data() {
     return {
-      oxygen: false
+      oxygen: false,
+      Gargles: false,
+      Medicine: false,
+      Exercises: false,
+      Temperature: false,
+      Pressure: false,
+      pulse: false
     }
   },
   computed: {
@@ -157,7 +149,13 @@ export default {
   watch: {
     currentDay(value){
       this.oxygen = this.$store.state.morning.oxygen[value]
-
+      this.Gargles   = this.$store.state.morning.Gargles[value]
+      this.Medicine  = this.$store.state.morning.Medicine[value]
+      this.Exercises  = this.$store.state.morning.Exercises[value]
+      this.Temperature  = this.$store.state.morning.Temperature[value]
+      this.Pressure  = this.$store.state.morning.Pressure[value]
+      this.pulse  = this.$store.state.morning.pulse[value]
+      this.Breakfast  = this.$store.state.morning.Breakfast[value]
     },
     oxygen(value){
       this.$store.commit('updateOxygen',{day: this.currentDay, value})
