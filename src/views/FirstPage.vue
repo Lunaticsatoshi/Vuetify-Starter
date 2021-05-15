0aba4fc45a92989cd3657d82de5a942c70264a93
vik17ag <vik17ag@gmail.com>
2021-05-15 18:07:09 +0530

    faet: Adds first pg

---
+++ src/views/FirstPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="my-5 mx-auto">
+    <h3>Welcome,</h3>
+    <v-text-field
+        label="Enter Your Name..."
+        class="mt-6"
+        block
+        placeholder="your name"
+        outlined
+    ></v-text-field>
+    <v-container>
+      <div class="about">
+        <h3> You are NOT ALONE and this is not the end of your story. BE BRAVE. BE STRONG</h3>
+      </div>
+    </v-container>
+    <v-btn block absolute bottom class="appBar">Next</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
